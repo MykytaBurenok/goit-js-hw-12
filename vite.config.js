4a547c568793c3b2b0502d72857474b871724e04
Mykyta Burenok <burenoknikitad@gmail.com>
2026-05-11 13:41:58 +0200

    Change base path in vite.config.js
    Update base path for the project configuration.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/goit-js-hw-12/',  // ← ДОДАЙ ЦЮ СТРОЧКУ!
+    base:'/image-search-gallery/',
     
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
